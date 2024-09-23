c7a2fa01a0c621b9e0593ad05bba4176387cce8e
taotieren <admin@taotieren.com>
2024-09-23 17:32:57 +0800

    Update cmake

--- Source/PurC/PlatformLinux.cmake
+++ Source/PurC/PlatformLinux.cmake
@@ -1,46 +1,37 @@
 set(PurC_OUTPUT_NAME purc)
 
-list(APPEND PurC_PRIVATE_INCLUDE_DIRECTORIES
-    "${PURC_DIR}/fetchers"
-    "${PURC_DIR}/fetchers/ipc"
-    "${PURC_DIR}/fetchers/ipc/unix"
-    "${PURC_DIR}/fetchers/launcher"
-    "${PURC_DIR}/fetchers/messages"
-    "${PURC_DIR}/fetchers/messages/soup"
+list(
+  APPEND
+  PurC_PRIVATE_INCLUDE_DIRECTORIES
+  "${PURC_DIR}/fetchers"
+  "${PURC_DIR}/fetchers/ipc"
+  "${PURC_DIR}/fetchers/ipc/unix"
+  "${PURC_DIR}/fetchers/launcher"
+  "${PURC_DIR}/fetchers/messages"
+  "${PURC_DIR}/fetchers/messages/soup"
+  "${GLIB_INCLUDE_DIRS}")
 
-    "${GLIB_INCLUDE_DIRS}"
-)
+list(APPEND PurC_UNIFIED_SOURCE_LIST_FILES "SourcesLinux.txt")
 
-list(APPEND PurC_UNIFIED_SOURCE_LIST_FILES
-    "SourcesLinux.txt"
-)
+list(APPEND PurC_SOURCES "${PURC_DIR}/ports/posix/rwlock.c"
+     "${PURC_DIR}/ports/posix/mutex.c" "${PURC_DIR}/ports/posix/sleep.c"
+     "${PURC_DIR}/ports/posix/file.c")
 
-list(APPEND PurC_SOURCES
-    "${PURC_DIR}/ports/posix/rwlock.c"
-    "${PURC_DIR}/ports/posix/mutex.c"
-    "${PURC_DIR}/ports/posix/sleep.c"
-    "${PURC_DIR}/ports/posix/file.c"
-)
+list(
+  APPEND
+  PurC_LIBRARIES
+  ${GLIB_GIO_LIBRARIES}
+  ${GLIB_GOBJECT_LIBRARIES}
+  ${GLIB_LIBRARIES}
+  ${GLIB_GMODULE_LIBRARIES}
+  -lm
+  -ldl
+  -lrt)
 
-list(APPEND PurC_LIBRARIES
-    ${GLIB_GIO_LIBRARIES}
-    ${GLIB_GOBJECT_LIBRARIES}
-    ${GLIB_LIBRARIES}
-    ${GLIB_GMODULE_LIBRARIES}
-    -lpthread
-    -lm
-    -ldl
-    -lrt
-)
-
-if (ENABLE_DNSSD)
-    list(APPEND PurC_LIBRARIES
-        ${DNSSD_LIBRARIES}
-    )
-endif ()
+if(ENABLE_DNSSD)
+  list(APPEND PurC_LIBRARIES ${DNSSD_LIBRARIES})
+endif()
 
 configure_file(ports/linux/purc.pc.in ${PurC_PKGCONFIG_FILE} @ONLY)
 install(FILES "${PurC_PKGCONFIG_FILE}"
-        DESTINATION "${LIB_INSTALL_DIR}/pkgconfig"
-)
-
+        DESTINATION "${LIB_INSTALL_DIR}/pkgconfig")
